95b9c8579598dbcf99adacf131efd304e4a7dc2c
re1laka <datvippro762@gmail.com>
2025-01-25 22:25:37 +0700

    Initialize patch-1

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hello bạn tui có điều muốn nói. </br> Tui Xin Lũi Bạn Mà!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
+  buttonYes: " đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn cậu.",
+  descriptionModar:
+    "À vậy là bạn chịu tha lỗi cho tui rồi tui hứa sẽ không làm vậy nữa đâu tin này tin chuẩn.</br> Love you <3",
+};
